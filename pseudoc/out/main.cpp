cefe3a0a67cc442e8501f6a27caf8820e7224fdb
Elías Josué Puma Chávez <elpumitaelias@gmail.com>
2017-10-06 15:46:52 -0500

    First compiling tasks

--- pseudoc/out/main.cpp
+++ pseudoc/out/main.cpp
@@ -5,5 +5,6 @@ int main() {
 	int a, b, c;
 	cin >> a;
 	cin >> b;
-	cout << 
+	c = a + b;
+	cout << c;
 }
--- pseudoc/src/main.rs
+++ pseudoc/src/main.rs
@@ -14,14 +14,14 @@ fn main() {
 
 	{
 		let mut line = line_it.next().unwrap().unwrap();
-		if let Some(_) = line.find("Inicio") {
+		if line.find("Inicio").is_some() {
 			writeln!(output, "int main() {{").expect("Error writing in file");
 		} else {
 			panic!("Not valid syntax");
 		}
 
 		line = line_it.next().unwrap().unwrap();
-		if let Some(_) = line.find("Variables") {
+		if line.find("Variables").is_some() {
 			line = line_it.next().unwrap().unwrap();
 			let mut vars: Vec<&str> = line.split(':').collect();
 			let var_type = vars.pop().unwrap();
@@ -31,29 +31,43 @@ fn main() {
 
 			match var_type.trim() {
 				"entero" => write!(output, "\tint ").expect("Error writing in file"),
-				"real"	=> write!(output, "\tfloat ").expect("Error writing in file"),
+				"real" => write!(output, "\tfloat ").expect("Error writing in file"),
 				_ => write!(output, "\ttype ").expect("Error writing in file"),
 			}
 
 			writeln!(output, "{};", vars[0]).expect("Error writing in file");
-			
+
 		} else {
 			panic!("Not valid syntax");
 		}
 	}
-	
+
 	for line in line_it {
 		let line = line.unwrap();
-		let mut statement: Vec<&str> = line.split_whitespace().collect();
+		let statement: Vec<&str> = line.split_whitespace().collect();
 		let keyword = statement[0];
 		println!("{}", keyword);
 		match keyword.trim() {
 			"Leer" => {
-				writeln!(output, "\tcin >> {};", statement[1]).expect("Error writing in file");
+				writeln!(output, "\tcin >> {};", statement[1]).expect("Error writing in file")
 			}
-			"Escribir" => writeln!(output, "\tcout").expect("Error writing in file"),
-			"Fin" => writeln!(output, "}}").expect("Error writing in file"),
-			_ => {}
+			"Escribir" => {
+				writeln!(output, "\tcout << {};", statement[1]).expect("Error writing in file")
+			}
+			"Fin" => {
+				writeln!(output, "}}").expect("Error writing in file");
+				break;
+			}
+			_ => match statement[1] {
+				"<-" => {
+					write!(output, "\t{} =", statement[0]).expect("Error writing in file");
+					for elem in statement.iter().skip(2) {
+						write!(output, " {}", elem).expect("Error writing in file");
+					}
+					writeln!(output, ";").expect("Error writing in file");
+				}
+				_ => println!("Simbolo no reconocido"),
+			},
 		}
 	}
 }
