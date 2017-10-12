90684bef61ae80554744fcbb79efd8489dde4cb6
Elías Josué Puma Chávez <elpumitaelias@gmail.com>
2017-10-11 21:06:36 -0500

    Transition table validator finished

--- 00-automata/src/main.rs
+++ 00-automata/src/main.rs
@@ -1,7 +1,9 @@
+#[derive(PartialEq, Clone, Copy)]
 enum State {
     One,
     Two,
     Three,
+    Error,
 }
 
 use State::*;
@@ -17,12 +19,12 @@ fn transition_diagram(input: &str) -> bool {
             } else {
                 return false;
             },
-            Two => return false,
             Three => if c.is_alphanumeric() {
                 state = Three;
             } else {
                 return false;
             },
+            _ => return false,
         }
     }
 
@@ -30,6 +32,43 @@ fn transition_diagram(input: &str) -> bool {
         Three => true,
         _ => false,
     }
+}
+
+enum Entry {
+    Letter,
+    Digit,
+    // FDC,
+}
+
+static TRANS_TBL: [[State; 3]; 3] = [
+    [Three, Two, Error],
+    [Error, Error, Error],
+    // [Three, Three, Accept],
+    [Three, Three, Three],
+];
+
+use Entry::*;
+
+fn transition_table(input: &str) -> bool {
+    let mut state = One;
+
+    for c in input.chars() {
+        let entry = if c.is_alphabetic() {
+            Letter
+        } else if c.is_numeric() {
+            Digit
+        } else {
+            return false;
+        };
+
+        state = TRANS_TBL[state as usize][entry as usize];
+
+        if state == Error {
+            return false;
+        }
+    }
+
+    true // state: accept
 }
 
 fn main() {
@@ -40,6 +79,14 @@ fn main() {
     assert_eq!(false, transition_diagram("~c"));
     assert_eq!(false, transition_diagram("123"));
     assert_eq!(true, transition_diagram("ccc"));
+
+    assert_eq!(true, transition_table("aaa"));
+    assert_eq!(false, transition_table("1a"));
+    assert_eq!(false, transition_table("^a"));
+    assert_eq!(true, transition_table("A2B"));
+    assert_eq!(false, transition_table("~c"));
+    assert_eq!(false, transition_table("123"));
+    assert_eq!(true, transition_table("ccc"));
 
     println!("Hello, world!");
 }
