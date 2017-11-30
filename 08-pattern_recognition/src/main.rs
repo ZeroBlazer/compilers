d3d6d1317cfe59482f2534de44214fdb45d4d09f
Elias J. Puma <elpumitaelias@gmail.com>
2017-11-30 10:39:02 -0500

    finished

--- 08-pattern_recognition/src/main.rs
+++ 08-pattern_recognition/src/main.rs
@@ -30,29 +30,39 @@ fn main() {
     let mut counter = 0;
     let mut current_state = A;
 
-    let (init, end) = (0, 0);
+    let mut range = (0, 0);
 
     for (i, c) in buffer.chars().enumerate() {
         let c = c.unwrap();
-        println!("'{}'", c);
+        // println!("'{}'", c);
 
         match current_state {
-            A => current_state = if c == '1' { B } else { A },
-            B => current_state = if c == '1' { C } else { A },
-            C => current_state = if c == '0' { D } else { C },
+            A => {
+                range.0 = i + 1;
+                current_state = if c == '1' { B } else { A };
+            }
+            B => {
+                current_state = if c == '1' { C } else { A };
+            }
+            C => {
+                current_state = if c == '0' { D } else { C };
+            }
             D => if c == '1' {
                 current_state = E;
                 counter += 1;
+                range.1 = i + 1;
+                println!("Found pattern from: {:?}", range);
             } else {
                 current_state = A;
             }
-            E => current_state = if c == '1' { C } else { A },
+            E => if c == '1' {
+                current_state = C;
+                range.0 = i;
+            } else {
+                current_state = A;
+            }
         }
     }
-
-    // while let Some(c) = it.next() {
-        
-    // }
 
     println!("Counter: {}", counter);
 }
