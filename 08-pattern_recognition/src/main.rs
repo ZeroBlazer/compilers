045b717bc581d3a89f794482727af3ffa2e27203
Elias J. Puma <elpumitaelias@gmail.com>
2017-11-30 15:29:22 -0500

    New architecture with 'matrix'

--- 08-pattern_recognition/src/main.rs
+++ 08-pattern_recognition/src/main.rs
@@ -1,6 +1,8 @@
 #![feature(io)]
 use std::io::{stdin, BufRead, BufReader, Read};
 use std::fs::File;
+use std::collections::BTreeMap;
+use std::collections::btree_map::Entry::Vacant;
 
 // fn get_input() -> String {
 //     let mut buffer = String::new();
@@ -12,57 +14,55 @@ use std::fs::File;
 //     buffer
 // }
 
-enum State {
-    A,
-    B,
-    C,
-    D,
-    E
-}
-
-use State::*;
+// use State::*;
 
 fn main() {
     let path = "../res/in/hw08_01";
     let input = File::open(path).expect("Unable to open");
     let buffer = BufReader::new(input);
 
-    let mut counter = 0;
-    let mut current_state = A;
+    let pattern = "1101";
+    let transtitions = ['0', '1'];
+    let n_states = pattern.len() + 1;
 
-    let mut range = (0, 0);
+    let mut matrix: BTreeMap<usize, BTreeMap<char, usize>> = BTreeMap::new();
 
-    for (i, c) in buffer.chars().enumerate() {
-        let c = c.unwrap();
-        // println!("'{}'", c);
+    // (0..n_states).for_each(|x| {
+    //     matrix.insert(x, BTreeMap::new());
+    // });
 
-        match current_state {
-            A => {
-                range.0 = i + 1;
-                current_state = if c == '1' { B } else { A };
-            }
-            B => {
-                current_state = if c == '1' { C } else { A };
-            }
-            C => {
-                current_state = if c == '0' { D } else { C };
-            }
-            D => if c == '1' {
-                current_state = E;
-                counter += 1;
-                range.1 = i + 1;
-                println!("Found pattern from: {:?}", range);
-            } else {
-                current_state = A;
-            }
-            E => if c == '1' {
-                current_state = C;
-                range.0 = i;
-            } else {
-                current_state = A;
-            }
+    let initial_trans = pattern.chars().nth(0).unwrap();
+
+    (0..n_states).for_each(|state| {
+        let inputs = matrix.entry(state).or_insert(BTreeMap::new());
+        
+        if state != n_states - 1 {
+            inputs.insert(pattern.chars().nth(state).unwrap(), state + 1);
         }
-    }
+
+        transtitions.iter().for_each(|t| {
+            inputs.entry(*t).or_insert(if *t == initial_trans {
+                matrix[&0][&initial_trans]
+            } else {
+                0
+            });
+        });
+    });
+
+    println!("{:#?}", matrix);
+
+    let mut counter = 0;
+    // let current_state = 0;
+    // let mut range = (0, 0);
+
+    // for (i, c) in buffer.chars().enumerate() {
+    //     let c = c.unwrap();
+    //     // println!("'{}'", c);
+
+    //     // match counter {
+            
+    //     // }
+    // }
 
     println!("Counter: {}", counter);
 }
